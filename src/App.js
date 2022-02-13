ba91e8a862227903796bcf0bd69f91e0900fa1ae
Rajeev <rranjan.ra15@gmail.com>
2022-02-13 17:38:25 +0530

    trial css part(grid)

--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
- 
-  const [endPoint,setEndPoint] = useState('')
+  const [endPoint, setEndPoint] = useState("");
 
-  const [container,setContainer] = useState([])
+  const [container, setContainer] = useState([]);
 
-  const [finalPoint,setFinalPoint] = useState('')
-  
-  useEffect(()=>{
-    fetchMe()
-  },[finalPoint])
+  const [finalPoint, setFinalPoint] = useState("");
 
-  const fetchMe = () =>{
+  useEffect(() => {
+    fetchMe();
+  }, [finalPoint]);
 
+  const fetchMe = () => {
     fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, {
-      "method": "GET",
-      "headers": {
+      method: "GET",
+      headers: {
         "x-rapidapi-host": "imdb8.p.rapidapi.com",
-        "x-rapidapi-key": "da661d6ee7msh0832edfa66b8cfcp17ed83jsn2342c8175d2c"
-      }
+        "x-rapidapi-key": "da661d6ee7msh0832edfa66b8cfcp17ed83jsn2342c8175d2c",
+      },
     })
-    .then(response => {
-      return response.json();
-    })
-    .then(data =>{
-      setContainer(data.d)
-    })
-    .catch(err => {
-      console.error(err);
-    });
-}
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setContainer(data.d);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-const onChangeHandler = (e) =>{
-  setEndPoint(e.target.value)
-}
+  const onChangeHandler = (e) => {
+    setEndPoint(e.target.value);
+  };
 
-const submitHandler = (e) =>{
-  e.preventDefault()
-  setFinalPoint(endPoint)
-}
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setFinalPoint(endPoint);
+  };
 
   return (
     <div className="App">
-     <form onSubmit={submitHandler}>
-       <input type="text" value={endPoint} onChange={onChangeHandler} />
-       <button type='submit'>submit</button>
-     </form>
-     {
-       container.map((item)=>{
-         return <div>
-          <img src={item.i.imageUrl} alt='images' /> 
-         <p>{item.l}</p>
-         </div>
-       })
-     }
+      <div className="header">
+        <h1>Movies Hub</h1>
+      </div>
+      <div>
+        <form onSubmit={submitHandler} className="form">
+          <input
+            type="text"
+            placeholder="search something..."
+            value={endPoint}
+            onChange={onChangeHandler}
+          />
+          <button type="submit">search</button>
+        </form>
+        <div className="cards-container" >
+          {container.map((item) => {
+            return (
+              <div className="cards">
+                <img src={item.i.imageUrl} alt="images" />
+                <p>{item.l}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
